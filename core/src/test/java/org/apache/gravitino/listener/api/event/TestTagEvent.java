aee2b29b7d7af6bc140ee2722a4d5da4e00600d1
Babu Mahesh <prbabumahesh@gmail.com>
2026-04-09 23:25:26 +0530

    Revert "[#10629]Improvement(core): Add tagName field and getter to GetTagForMetadata (#10651)"
    This reverts commit 64021c13f8614d2ec63387a378137570bb6329ec.

--- core/src/test/java/org/apache/gravitino/listener/api/event/TestTagEvent.java
+++ core/src/test/java/org/apache/gravitino/listener/api/event/TestTagEvent.java
@@ -372,7 +372,6 @@ public class TestTagEvent {
         ((GetTagForMetadataObjectFailureEvent) event).exception().getClass());
     Assertions.assertEquals(OperationType.GET_TAG_FOR_METADATA_OBJECT, event.operationType());
     Assertions.assertEquals(OperationStatus.FAILURE, event.operationStatus());
-    Assertions.assertEquals(tag.name(), ((GetTagForMetadataObjectFailureEvent) event).tagName());
   }
 
   @Test
